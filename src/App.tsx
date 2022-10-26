6189b3d59931063a97f65f7c4f15ac386a8d67c4
TomasSadone <tomisadone27@gmail.com>
2022-10-26 20:44:43 -0300

    finished footer and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DecorativeImages from "./components/DecorativeImages";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
@@ -13,6 +14,7 @@ const App = (props: Props) => {
       <Services />
       <Testimonials />
       <DecorativeImages />
+      <Footer />
     </div>
   );
 };
